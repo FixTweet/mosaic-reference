29d64ab1d87c11e80d95b002c61aa8321a80ceba
dangered wolf <d@ngeredwolf.me>
2022-07-20 19:47:27 -0400

    Added additional headers

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,8 @@ import got from 'got';
 import 'dotenv/config';
 import { mosaic } from './mosaic.js';
 
+const fakeChromeVersion = '103';
+
 // Basic express setup
 const app = express();
 
@@ -19,7 +21,22 @@ app.get('/', async (req, res) => {
         const response = await got(`https://pbs.twimg.com/media/${image}?format=png&name=large`, {
           responseType: 'buffer',
           headers: {
-            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.149 Safari/537.36'
+            'sec-ch-ua': `".Not/A)Brand";v="99", "Google Chrome";v="${fakeChromeVersion}", "Chromium";v="${fakeChromeVersion}"`,
+            'DNT': `1`,
+            'x-twitter-client-language': `en`,
+            'sec-ch-ua-mobile': `?0`,
+            'content-type': `application/x-www-form-urlencoded`,
+            'User-Agent': `Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/${fakeChromeVersion}.0.0.0 Safari/537.36`,
+            'x-twitter-active-user': `yes`,
+            'sec-ch-ua-platform': `"Windows"`,
+            'Accept': `*/*`,
+            'Origin': `https://twitter.com`,
+            'Sec-Fetch-Site': `same-site`,
+            'Sec-Fetch-Mode': `cors`,
+            'Sec-Fetch-Dest': `empty`,
+            'Referer': `https://twitter.com/`,
+            'Accept-Encoding': `gzip, deflate, br`,
+            'Accept-Language': `en`
           }
         });
         downloadedImages[images.indexOf(image)] = response.body;
@@ -36,6 +53,7 @@ app.get('/', async (req, res) => {
     // Send buffer with webp mime
     res.setHeader('Content-Type', 'image/webp');
     res.setHeader('Cache-Control', 'public, max-age=31536000');
+    res.setHeader('X-Powered-By', 'pxTwitter-Mosaic');
     res.send(result);
   } catch (e) {
     console.log(e);
